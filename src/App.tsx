35778c0a2dbd56b2bbc77e4fe95b71b9c0b6e8e7
abdellaheltrach <abdellah.eltrach@gmail.com>
2025-11-21 22:01:32 +0100

    feat: Add TypeScript support with new configurations, dependencies, and initial components, and migrate Vite config to TypeScript.

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        <h1>hello world!!</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -22,7 +23,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
